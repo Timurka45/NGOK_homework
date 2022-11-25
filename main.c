9bcdd26bfd90d708f2c589fec56ef81a33e7beba
NGOK-Chernenko <116289378+NGOK-Chernenko@users.noreply.github.com>
2022-11-25 13:58:03 +0700

    Update main.c
    =(

--- main.c
+++ main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
+
 #define SIZE 5
+
 int main(void)
 {
 int m[SIZE]=  {0};
+  // sum может содержать в себе мусор без явленого присваивания значения
 int sum;
+  
   for(int i = 0; i < SIZE; ++i)
   {
     scanf("%d", &m[i]);
   }
+  
     for(int i = 0; i < SIZE; ++i)
   {
     printf("[%d]", m[i]);
-  }
+  }  
   printf("\n");
+  
+  // значение i смени в зависимости от SIZE
     for(int i = 4; i >= 0; --i)
   {
     printf("[%d]", m[i]);
   }
     printf("\n");
+  
     for(int i = 0; i < SIZE; ++i)
   {
     if (i % 2 == 0) {
@@ -25,6 +33,7 @@ int sum;
     }
   }
   printf("\n");
+  
     for(int i = 0; i < SIZE; ++i)
   {
     if (i % 2 > 0) {
