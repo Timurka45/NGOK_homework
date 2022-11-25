4a31375b8224bb853d02840e0b22a2d3263a9f65
Timurka45 <118248213+Timurka45@users.noreply.github.com>
2022-11-25 15:38:44 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,50 +1,19 @@
-#include <stdio.h>
-
-#define SIZE 5
-
-int main(void)
+#include <stdio.h> //подключает библиотеку
+// библиотека ввода-вывода
+//stdin, stdout, stderror
+// int - тип данных(целое число) %d, char-символ %c, float & double - 3.14 %f %lf
+int main(void)//главная функция
 {
-int m[SIZE]=  {0};
-  // sum может содержать в себе мусор без явленого присваивания значения
-int sum =0;
-  
-  for(int i = 0; i < SIZE; ++i)
-  {
-    scanf("%d", &m[i]);
-  }
-  
-    for(int i = 0; i < SIZE; ++i)
-  {
-    printf("[%d]", m[i]);
-  }  
-  printf("\n");
-  
-  // значение i смени в зависимости от SIZE
-    for(int i = SIZE-1; i >= 0; --i)
-  {
-    printf("[%d]", m[i]);
-  }
-    printf("\n");
-  
-    for(int i = 0; i < SIZE; ++i)
-  {
-    if (i % 2 == 0) {
-    printf("[%i]", m[i]);
-    }
-  }
-  printf("\n");
-  
-    for(int i = 0; i < SIZE; ++i)
-  {
-    if (i % 2 > 0) {
-    printf("[%i]", m[i]);
-    }
-  }
-    for(int i = 0; i < SIZE; ++i)
-  {
-    sum += m[i];
-  }
-  printf("\nсумма = %d", sum);
+int count;//объявление переменной
+count = 5;//присваивание значения
+//у инта диапозон значений от короче много
+unsigned int new_count;//объявили переменную
+new_count = 3000000000;// = это оператор присваивания, от 0 до 4294000000
+int check=2,kek=1;//вот так можно тоже
+char a = 'A';
+a = 37;
+double b = 3.14;
+printf("%lf", b);
 
-}
+}// выполняется досюда
 
